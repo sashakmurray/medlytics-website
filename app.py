27d47d03035dac6526908a4c14f1f622234da1bf
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2023-08-01 15:34:32 -0400

    Add image upload and theme

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hello world")
+import streamlit as st
+
+def main():
+    st.title("AutiVisi∞n")
+
+    st.subheader("Please upload an image of a face below:")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+        st.write("The image above is an example of a lemniscate.")
+
+main()
