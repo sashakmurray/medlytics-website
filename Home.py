9ca7f320aa7ca54a47b3802d5e91bc4d6d4a7dab
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2023-08-02 10:57:52 -0400

    Made app multi-page

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def main():
+    st.title("L∞mnisceight")
+
+main()
--- pages/1_Autivision.py
+++ pages/1_Autivision.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="My Page Title", page_icon="👋")
 
 def main():
     st.title("AutiVisi∞n")
