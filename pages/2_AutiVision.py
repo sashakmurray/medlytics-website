702453e6b6f2ef5ae31aaca238f4e158a0d52f45
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2023-08-04 10:58:04 -0400

    Finish autivision page

--- pages/2_AutiVision.py
+++ pages/2_AutiVision.py
@@ -11,6 +11,7 @@ def main():
     st.subheader("Please upload an image of a face below:")
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    st.write("The model may take a few minutes to run.")
 
     if uploaded_file is not None:
         # st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
@@ -19,9 +20,9 @@ def main():
         image = image[:, :, :3]
         image = np.reshape(image, (1, 300, 300, 3))
         image = image.astype(np.float32) / 255.0
-        model = keras.models.load_model("model_V2/")
+        model = keras.models.load_model("tools/model_V2/")
         predictions = model.predict(image)
-        st.write(f"Our model predicts a {predictions[0][0] * 100}% chance of autism in this photo.")
+        st.write(f"Our model predicts a {round(predictions[0][0] * 100, 4)}% chance of autism in this photo.")
 
 
 main()
