41777ebd1a0c648de8caaa35e1d142101b55de65
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2023-08-06 11:45:33 -0400

    Add description to home page

--- Home.py
+++ Home.py
@@ -2,5 +2,14 @@ import streamlit as st
 
 def main():
     st.title("L∞mnisceight")
+    st.write("We are L∞mnisceight, a group committed to increasing accessibility for autism detection and treatment.")
+    st.write("Autism is a neurological and developmental condition that affects an individual's behavior, social interaction, and sensory processing; the global estimate suggests that around 75 million people have autism.")
+    st.write("""In the United States, approximately 2-3% of children have been diagnosed with autism, but a considerable proportion, especially among minorities, remain undiagnosed.
+Early diagnosis and support for children with autism is paramount, as this plays a pivotal role in helping them adapt to social settings, ultimately leading to improved long-term outcomes.
+To address the diagnostic disparity caused by socioeconomic factors, we have developed an image-based model to identify autism in children with an emphasis on accessibility.
+Our primary objective was to take steps in the direction of ensuring that all children have access to accurate autism diagnosis.
+""")
+    st.write("""Autism can often present challenges, such as aggression, irritability, and self-injurious behaviors, which can significantly impact an individual’s daily life. 
 
+We developed a model to expedite this process of drug discovery. Using our model, a researcher could input the molecular structure of a drug candidate, and our model would return information about the candidate’s efficacy in specifically addressing aggression and self-injurious behaviors in individuals with autism.""")
 main()
